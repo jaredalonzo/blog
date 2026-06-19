cc4e9fe257d03d1d8e7a7b38368547c085d178f8
Jared Alonzo <jared.alonzo@icloud.com>
2026-06-19 14:04:57 -0400

    Add Vercel Web Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { ThemeToggle } from "@/components/ThemeToggle";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -61,6 +62,7 @@ export default function RootLayout({
             <ThemeToggle />
           </div>
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
